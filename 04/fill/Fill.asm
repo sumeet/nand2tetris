f9b89e3acdee4c9d1d4c668ef2358f651c16f57c
Sumeet Agarwal <sumeet.a@gmail.com>
2022-02-09 22:26:07 -0800

    04: assembly programs done!
    i hate writing assembly

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,64 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// total number of pixels: 512w * 256h = 131072 pixels (bits)
+// number of 16-bit addresses: 8192
+(LOOP)
+// store this in M[0]          ^
+@8192
+D=A
+@0
+M=D
+
+// load KBD into D
+@KBD
+D=M
+
+// if no key pressed (KBD is 0), then fill with white
+@WHITE
+D;JEQ
+
+// else fill with black
+(BLACK)
+@0
+D=M
+
+// go back to input loop if done drawing pixels
+@LOOP
+D;JEQ
+
+// subtract num pixels left to draw from screen
+// screen is: 16384
+// screen max is: 16384 + 8192 = 24575
+@24576
+A=A-D
+// -1 = every bit flipped to 1 (black)
+M=-1
+
+@0
+M=M-1
+
+@BLACK
+0;JMP
+
+(WHITE)
+@0
+D=M
+
+// go back to input loop if done drawing pixels
+@LOOP
+D;JEQ
+
+// subtract num pixels left to draw from screen
+// screen is: 16384
+// screen max is: 24576 + 8192 = 24575
+@24576
+A=A-D
+// 0 = white
+M=0
+
+@0
+M=M-1
+
+@WHITE
+0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,4 +9,35 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+
+// initialize product to 0, as per test
+@2
+M=0
+
+(LOOP)
+// store R1 into register D, the number of times to add
+@1
+D=M
+
+// if number of times to add is zero, then we're done, jump to the end
+@END
+D;JEQ
+
+// add the value being multipled into R2
+@0
+D=M
+@2
+M=M+D
+
+// decrement the number of times to add by 1
+@1
+M=M-1
+
+@LOOP
+0;JMP
+
+
+(END)
+(INFINITE_LOOP)
+@INFINITE_LOOP // Infinite loop (our standard
+0;JMP // way to terminate programs).
